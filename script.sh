7db1e815bbae0f29e5d0371be294e89a13eb906c
Nellyth Arroyo Marrugo <nellith25@gmail.com>
2023-11-08 21:37:00 -0500

    Actualizaci├│n autom├ítica

--- script.sh
+++ script.sh
@@ -12,48 +12,25 @@ fi
 # Moverse a la carpeta del repositorio clonado
 cd $WORK_DIR
 
-# Dar la opción de hacer "push" o "pull"
-echo "Seleccione una opción:"
-echo "1. Hacer 'push' al repositorio"
-echo "2. Hacer 'pull' desde el repositorio"
-read -p "Opción (1/2): " option
+# Lee todas las líneas del archivo "server.config" en una matriz
+readarray -t server_lines < "$WORK_DIR/server.config"
 
-if [ "$option" -eq 1 ]; then  
-    # Realizar "push" al repositorio
-    git checkout -b test
-    git pull test
-    git add .
-    git commit -m "Actualización automática"
-    git push --set-upstream origin test
-    git checkout main
-    git branch -D test
-elif [ "$option" -eq 2 ]; then
-    # Realizar "pull" desde el repositorio
-    git checkout main
-    git pull
-
-    # Lee todas las líneas del archivo "server.config" en una matriz
-    readarray -t server_lines < "$WORK_DIR/server.config"
-
-    # Itera a través de las líneas almacenadas en la matriz
-    for server in "${server_lines[@]}"; do
-        # Haces lo que necesites con cada línea, por ejemplo:
-    if [ ! -d "$server" ]; then
-            echo "No se encuentra la carpeta de Minecraft del Servidor."
-        else
-            rm -rf "$server/mods"
-            
-            cp -r "$WORK_DIR/mods" "$server/mods"
-            cp -r "$WORK_DIR/CustomSkinLoader" "$server/CustomSkinLoader"
-            cp -r "$WORK_DIR/server.properties" "$server/server.properties"
-            cp -r "$WORK_DIR/server-icon.png" "$server/server-icon.png"
-            cp -r "$WORK_DIR/world" "$server/world"
-            cp -r "$WORK_DIR/config" "$server/config"
-            sudo chmod 777 -R $server
-       fi
-    done
-else
-    echo "Opción no válida. Saliendo."
-fi
+# Itera a través de las líneas almacenadas en la matriz
+for server in "${server_lines[@]}"; do
+	# Haces lo que necesites con cada línea, por ejemplo:
+	if [ ! -d "$server" ]; then
+		echo "No se encuentra la carpeta de Minecraft del Servidor."
+	else
+		rm -rf "$server/mods"
+		
+		cp -r "$WORK_DIR/mods" "$server/mods"
+		cp -r "$WORK_DIR/CustomSkinLoader" "$server/CustomSkinLoader"
+		cp -r "$WORK_DIR/server.properties" "$server/server.properties"
+		cp -r "$WORK_DIR/server-icon.png" "$server/server-icon.png"
+		cp -r "$WORK_DIR/world" "$server/world"
+		cp -r "$WORK_DIR/config" "$server/config"
+		sudo chmod 777 -R $server
+   fi
+done
 
 read -p "Presiona Enter para continuar..."
